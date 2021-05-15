8fd02fea970cac8654ae7be6508fba814ee16fb3
appcoreopc <jeremy@jeremys-MacBook-Pro.local>
2021-05-16 07:35:06 +0800

    added transaction support for block chain

--- block.go
+++ block.go
@@ -6,33 +6,51 @@ import (
 	"log"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 type BlockChain struct {
-	client ethclient.Client
+	client       ethclient.Client
+	currentBlock *types.Block
 }
 
-func New(serviceProviderUrl string) *ethclient.Client {
+func (b *BlockChain) ConnectServiceProvider(serviceProviderUrl string) *BlockChain {
 
 	client, err := ethclient.Dial("https://mainnet.infura.io/v3/ea7c5898898f4dcfa59ebe635621fbf8")
 	if err != nil {
 		log.Fatal(err)
 	}
-	return client
+
+	b.client = *client
+	return &BlockChain{}
 }
 
 func (b *BlockChain) GetBlockByNumber(blockNo int64) {
 
 	blockNumber := big.NewInt(blockNo)
 	block, err := b.client.BlockByNumber(context.Background(), blockNumber)
+	b.currentBlock = block
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(block.Number().Uint64())     // 5671744
-	fmt.Println(block.Difficulty().Uint64()) // 3217000136609065
-	fmt.Println(block.Hash().Hex())          // 0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9
-	fmt.Println(len(block.Transactions()))   // 144
+	fmt.Println(block.Number().Uint64())
+	fmt.Println(block.Difficulty().Uint64())
+	fmt.Println(block.Hash().Hex())
+	fmt.Println(len(block.Transactions()))
+}
+
+func (b *BlockChain) GetTransactions() {
+
+	for _, tx := range b.currentBlock.Transactions() {
+		fmt.Println(tx.Hash().Hex())
+		fmt.Println(tx.Value().String())
+		fmt.Println(tx.Gas())
+		fmt.Println(tx.GasPrice().Uint64())
+		fmt.Println(tx.Nonce())
+		fmt.Println(tx.Data())
+		fmt.Println(tx.To().Hex())
+	}
 }
