50abba21bc730e0e47c5ff94b45f815f850e15d4
appcoreopc <jeremy@jeremys-MacBook-Pro.local>
2021-05-21 14:31:37 +0800

    added first test case

--- block_test.go
+++ block_test.go
@@ -1,7 +1,40 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"math/big"
+	"testing"
 
-func TestAbc(t *testing.T) {
-	t.Error() // to indicate test failed
+	"github.com/smartystreets/assertions"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/stretchr/testify/mock"
+)
+
+/// Mock
+type ClientBlockMock struct {
+	mock.Mock
+}
+
+func (m *ClientBlockMock) BlockByNumber(context context.Context, blockNumber *big.Int) (*types.Block, error) {
+	args := m.Called(context, blockNumber)
+	return args.Get(0).(*types.Block), args.Error(1)
+}
+
+func WhenValidBlockNumberThenBlockDataReturns(t *testing.T) {
+
+	mock := ClientBlockMock{}
+	mock.On("BlockByNumber", context.Background(), 100).Return(nil, nil)
+
+}
+
+func WhenInvBlockNumberThenBlockDataReturns(t *testing.T) {
+
+	mock := ClientBlockMock{}
+	mock.On("BlockByNumber", context.Background(), 100).Return(nil, nil)
+
+	bService := NewBlockChainService(&mock)
+	bService.GetBlockByNumber(100)
+	assertions.ShouldBeNil(bService.client)
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/appcoreopc/blockG
 go 1.16
 
 require (
-	github.com/ethereum/go-ethereum v1.10.3 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/ethereum/go-ethereum v1.10.3
+	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d
+	github.com/stretchr/testify v1.7.0
 )
